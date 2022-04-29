8636a8077aae05f3809ce1ef7dc2e8885c511ac8
FilipKlominek <71869121+FilipKlominek@users.noreply.github.com>
2022-04-29 08:37:24 +0200

    Add files via upload

--- LightsOut2/src/main/kotlin/Main.kt
+++ LightsOut2/src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ fun main() {
     val game = Game()
     game.randomize()
     game.print()
+    val start = System.currentTimeMillis()
 
     while (!game.checkWin()) {
         println("enter y")
@@ -11,5 +12,7 @@ fun main() {
         if (game.click(y, x)) game.moveCount++
         game.print()
     }
+    val end = System.currentTimeMillis()
+    println("time: " + ((end.toDouble() - start.toDouble()) / 1000) + "s")
 
 }
